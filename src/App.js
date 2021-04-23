25d92eb12c80b031ac8176e74f8c4c57bf205dc4
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-23 08:04:50 -0600

    Added Home component and some css styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from 'react-router-dom';
 
+//Components imports
+import Home from './Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='App'>
+      <nav>
+        <h1 className='shop-header'>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+      </nav>
+      <Home />
+    </div>
   );
 };
 export default App;
