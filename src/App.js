f459cfaa54cd37da94b22b337078649c3d262ef8
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-23 11:35:06 -0600

    Added schema

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from 'react-router-dom';
+import * as yup from "yup";
+import schema from "./formSchema"
 
 //Components imports
 import Home from './Home';
@@ -28,7 +31,36 @@ const App = () => {
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
   const [disabled, setDisabled] = useState(initialDisabled);
-  
+
+  const postNewOrder = (newOrder) => {
+    axios.post("https://reqres.in/api/orders", newOrder)
+    .then((res) => {
+      setOrders([...orders, res.data]);
+      setFormValues(initialFormValues);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  const inputChange = () => {
+
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: ["cheese, pepperoni, mushroom, ham"].filter(
+        (item) => formValues[item]
+      ),
+      special: formValues.special.trim(),
+    };
+    postNewOrder(newOrder);
+  };
+
+  //Side Effects
+
   return (
     <div className='App'>
       <nav>
@@ -41,7 +73,13 @@ const App = () => {
 
       <Switch>
         <Route path='/pizza'>
-          <OrderForm />
+          <OrderForm 
+            values={formValues}
+            change={inputChange}
+            submit={formSubmit}
+            disabled={disabled}
+            errors={formErrors}
+          />
         </Route>
         <Route path='/'>
           <Home />
