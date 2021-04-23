1b2c23183055be257ed69620327ed0fbfd79e6a3
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-23 10:58:45 -0600

    Finished form and added initial form values and state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import OrderForm from './OrderForm';
 
+//Shape of state for form
+const initialFormValues = {
+  name: "",
+  size: "",
+  cheese: "",
+  pepperoni: false,
+  mushroom: false,
+  ham: false,
+  special: "",
+};
+
+const initialFormErrors = {
+  name: "",
+};
+const initialOrders = [];
+const initialDisabled = true;
+
+
 const App = () => {
+  const [orders, setOrders] = useState(initialOrders);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  
   return (
     <div className='App'>
       <nav>
--- src/OrderForm.js
+++ src/OrderForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function OrderForm() {
     return (
         <form id='pizza-form'>
-            <div className='form-inputs'>
+            <div className='first-input'>
                 <label>Name
                     <input id='name-input'
                       type="text"
@@ -13,7 +13,9 @@ export default function OrderForm() {
                       placeholder="Enter name here..."
                     />
                 </label>
+            </div>
 
+            <div className="dropdown">
                 <label>Size
                     <select id='size-dropdown'
                       value=""
@@ -25,6 +27,61 @@ export default function OrderForm() {
                         <option value="Large">Large</option>
                     </select>
                 </label>
+            </div>
+
+            <div className="checkboxes">
+                <label>
+                    Cheese
+                    <input
+                      type="checkbox"
+                      name="cheese"
+                      checked=""
+                      onChange=""
+                    />
+                </label>
+                <label>
+                    Pepperoni
+                    <input
+                      type="checkbox"
+                      name="pepperoni"
+                      checked=""
+                      onChange=""
+                    />
+                </label>
+                <label>
+                    Mushroom
+                    <input
+                      type="checkbox"
+                      name="mushroom"
+                      checked=""
+                      onChange=""
+                    />
+                </label>
+                <label>
+                    Ham
+                    <input
+                      type="checkbox"
+                      name="hame"
+                      checked=""
+                      onChange=""
+                    />
+                </label>
+            </div>
+
+            <div className="second-input">
+                <label>Special Instructions
+                    <input id='special-text'
+                      type="text"
+                      value=""
+                      onChange=""
+                      name="special"
+                      placeholder="Anything else?"
+                    />
+                </label>
+            </div>
+
+            <div>
+                <button id="order-button"disabled="">Add to Order</button>
             </div>
         </form>
     )
